fdaa3c4d31411fd88765c4f4838d129fe0fc304b
Lourdene Santos <2233120@slu.edu.ph>
2024-12-09 19:05:33 +0800

    add: Added genre field to Book structure

--- src/library.h
+++ src/library.h
@@ -5,11 +5,13 @@
 #define TITLE_LENGTH 100
 #define AUTHOR_LENGTH 50
 #define ISBN_LENGTH 20
+#define GENRE_LENGTH 30
 
 typedef struct {
     char title[TITLE_LENGTH];
     char author[AUTHOR_LENGTH];
     char isbn[ISBN_LENGTH];
+    char genre[GENRE_LENGTH];
 } Book;
 
 void addBook();
